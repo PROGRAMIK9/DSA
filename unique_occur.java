2597c05625893015a1894b6738aa3d0c8b35a00e
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2025-11-07 11:45:34 +0530

    Unique Occurences

---
+++ unique_occur.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+       Map <Integer, Integer> map = new HashMap<>();
+       for(int x:arr){
+        map.put(x,map.getOrDefault(x,0)+1);
+       } 
+       Set<Integer> s = new HashSet<>();
+       for(int x:map.values()){
+        s.add(x);
+       }
+       return map.size()==s.size();
+    }
+}
