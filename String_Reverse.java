6231ee7e568ce525138c1827308242ab73645b90
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2025-11-07 18:22:28 +0530

    string reverse

---
+++ String_Reverse.java
@@ -0,0 +1,13 @@
+class Strng_reverse {
+    public void reverseString(char[] s) {
+        int i=0, j = s.length-1;
+        while(i<j){
+            char temp;
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
