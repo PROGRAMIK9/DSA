a0fa183e97809bf296181c384a86b42d1b6e95bc
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2025-11-15 17:54:30 +0530

    number of elements appearing n/k times

---
+++ NumbersK.java
@@ -0,0 +1,18 @@
+class Solution {
+    // Function to find all elements in array that appear more than n/k times.
+    public int countOccurence(int[] arr, int k) {
+        // your code here,return the answer
+        Arrays.sort(arr);
+        int num=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)>arr.length/k){
+               num++;
+            }
+        }
+        return num;    
+    }
+}
