31f98abb5ebc2e78310c2e5ed75ef591fd8e5162
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2025-12-22 09:40:08 +0530

    Detect the first element of the loop in a linked list

---
+++ DetectFirstEleemntofLOop.java
@@ -0,0 +1,32 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+};
+*/
+
+class Solution {
+    public int cycleStart(Node head) {
+        // code here
+        Node slow = head, fast = head;
+        while(slow!=null && fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow = head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow.data;
+            }
+        }
+        return -1;
+    }
+}
