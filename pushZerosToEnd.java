8c929a4ccd4a8d15c0d9b4b694f652a7bf2b6986
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2026-02-03 18:54:32 +0530

    Implement pushZerosToEnd function to move all zeros in an array to the end

---
+++ pushZerosToEnd.java
@@ -0,0 +1,17 @@
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                swap(arr,i,count);
+                count++;
+            }
+        }
+    }
+    void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
