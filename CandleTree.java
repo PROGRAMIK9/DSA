8c0a98518c789cbe1788f281727154d457f812e1
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2026-03-18 11:18:36 +0530

    Candle Tree POTD

---
+++ CandleTree.java
@@ -0,0 +1,28 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    int moves = 0;
+    public int dfs(Node root){
+        if(root == null)return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        moves+=Math.abs(left)+Math.abs(right);
+        return root.data+left+right-1;
+    }
+    public int distCandy(Node root) {
+        // code here
+        dfs(root);
+        return moves;
+    }
+}
