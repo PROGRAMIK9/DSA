4232a02a3bcd127769d901bdc139aa90adcdcc7f
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2026-03-23 21:34:58 +0530

    Longest Cycle POTD

---
+++ LongestCycle.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestCycle(int v, int[][] edges){
+        int n=edges.length,res=-1;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<n;i++){
+            hm.put(edges[i][0],edges[i][1]);
+        }
+        for(int i=0;i<v;i++){
+            boolean vis[] = new boolean[v];
+            int p=i,c=0;
+            boolean de = false;
+            while(vis[p] == false){
+                vis[p]=true;
+                c++;
+                if(hm.containsKey(p)){
+                   p=hm.get(p);
+                }
+                else{
+                   de=true;
+                   break;
+                }
+            }
+            if(!de && p==i){
+               res=Math.max(res,c);
+            }
+        }
+        return res;
+    }
+}
