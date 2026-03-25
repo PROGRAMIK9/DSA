03e8943175e2b25dc6ed0e14b49b114f5dc720c4
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2026-03-25 14:40:32 +0530

    Search 2 D matrix PDT

---
+++ Search2Dmatrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]>target || matrix[i][m-1] < target ){
+                continue;
+            }else{
+                int start = 0;
+                int end = m-1;
+                while(start<=end){
+                    int mid =(start+end)/2;
+                    if(matrix[i][mid]==target)return true;
+                    else if(matrix[i][mid]>target) end = mid-1;
+                    else start = mid+1;
+                }
+            }
+        }    
+        return false;
+    }
+}
+    
