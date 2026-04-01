db6fee4356a79f758d9b192484bb0c3c92f4b8f6
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2026-04-01 09:04:56 +0530

    Find the binary combinations without 1

---
+++ NumOfnoOnecons.java
@@ -0,0 +1,14 @@
+class Solution {
+    int countStrings(int n) {
+        // code here
+        int f1 =0;
+        int f2 =1;
+        int f=0;
+        for(int i=1;i<=n+1;i++){
+            f=f1+f2;
+            f1=f2;
+            f2=f;
+        }
+        return f;
+    }
+}the 
