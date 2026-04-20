082eb796640ca6f28844a00842f53959c56a05dc
PROGRAMIK9 <aaryamannprabhu01@gmail.com>
2026-04-20 09:53:08 +0530

    Derange Count POTD

---
+++ IsPower.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPower(int x, int y) {
+        // code here
+        if(x==1) return y==1;
+        double res = Math.log(y)/Math.log(x);
+        return Math.abs(res-Math.round(res))<1e-10;
+    }
+}
